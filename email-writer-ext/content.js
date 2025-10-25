7d9296de8a100e196e1457e10321d760a56af3a8
Harshitha Shetty <141444342+HarshithaShetty27@users.noreply.github.com>
2025-10-25 15:32:40 +0530

    Extension- react

---
+++ email-writer-ext/content.js
@@ -0,0 +1,24 @@
+console.log("ComposeAI extension - Content script loaded.");
+
+function injectButton(){
+
+}
+
+const observer = new MutationObserver((mutations)=>{
+    for(const mutation of mutations){
+        const addedNodes = Array.from(mutation.addedNodes);
+        const hasComposeElements = addedNodes.some(node=>
+            node.nodeType === Node.ELEMENT_NODE &&
+            (node.matches('.aDh, .btC, [role="dialog"]') || node.querySelector('.aDh, .btC, [role="dialog"]'))
+        );
+        if(hasComposeElements){
+            console.log("ComposeAI extension - Detected compose elements added to the DOM.");
+            setTimeout(injectButton, 500);
+        }
+    }
+});
+
+observer.observe(document.body, {
+    childList: true,
+    subtree: true
+})
